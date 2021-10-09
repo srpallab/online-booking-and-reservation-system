7b1b6ed74d934b68b64a04a6c5b571aeb2c891f4
Shafiqur Rahman <pallab.alif@gmail.com>
2021-10-10 03:38:22 +0600

    ADD: table structure.

--- index.php
+++ index.php
@@ -11,9 +11,18 @@
     <div class="container mx-auto flex items-center flex-col h-screen justify-center">
       <h1 class="text-6xl text-center text-white">Online Reservation and Booking System</h1>
       <div class="grid grid-cols-3 gap-10 pt-10">
-	<div class="orbs-bg-img-car rounded-lg border-2 hover:border-green-700 h-60 w-96 transform hover:scale-110"></div>
-	<div class="orbs-bg-img-resort rounded-lg border-2 hover:border-green-700 h-60 w-96 transform hover:scale-110"></div>
-	<div class="orbs-bg-img-room rounded-lg border-2 hover:border-green-700 h-60 w-96 transform hover:scale-110"></div>
+	<a href="booking.php?resort=car">
+	  <div class="orbs-bg-img-car rounded-lg border-2 hover:border-green-700 h-60 w-96 transform hover:scale-110"></div>
+	  <h1 class="text-4xl text-white text-center mt-5">Vehicles Booking</h1>
+	</a>
+	<a href="booking.php?type=resort">
+	  <div class="orbs-bg-img-resort rounded-lg border-2 hover:border-green-700 h-60 w-96 transform hover:scale-110"></div>
+	  <h1 class="text-4xl text-white text-center mt-5">Resort Booking</h1>
+	</a>
+	<a href="booking.php?type=room">
+	  <div class="orbs-bg-img-room rounded-lg border-2 hover:border-green-700 h-60 w-96 transform hover:scale-110"></div>
+	  <h1 class="text-4xl text-white text-center mt-5">Hotel Room Booking</h1>
+	</a>
       </div>
     </div>
     <script src="https://kit.fontawesome.com/fc813874b8.js" crossorigin="anonymous"></script>
