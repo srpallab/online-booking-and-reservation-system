a85cf6c30e505781900a354b3285f18b5abb59c7
Shafiqur Rahman <pallab.alif@gmail.com>
2021-10-14 23:24:58 +0600

    REM: Add customer option.

--- inc/navbar.inc.php
+++ inc/navbar.inc.php
@@ -1,7 +1,6 @@
 <nav class="container mx-auto">
-  <ul class="flex justify-between items-center">
+  <ul class="flex justify-around items-center">
     <li class="py-2"><i class="fas fa-home pr-2"></i><a href="index.php">HOME</a></li>
-    <li class="py-2"><i class="fas fa-user-plus pr-2"></i><a href="">ADD CUSTOMER</a></li>
     <li class="py-2"><i class="fas fa-cash-register pr-2"></i><a href="">CHECK OUT</a></li>
   </ul>
 </nav>
